ea3fd253da142c18c1e6ada9f33332be5fda5795
Nic Schumann <nicolas_schumann@brown.edu>
2018-05-06 16:14:30 -0400

    [Contact Form] added an image for the contact form.

--- wp-content/themes/custom/partials/home/home_contact_slide.php
+++ wp-content/themes/custom/partials/home/home_contact_slide.php
@@ -1,15 +1,22 @@
 <div id="contact" class="home-slide row">
     <div class="home-slide-content-relative col-sm-6 offset-sm-1">
+        <div class="row">
+
+
         <div class="col-sm-6 offset-sm-3">
             <?php $title = array('english' => 'Contact', 'espanol' => 'Contacto'); ?>
             <?php $tag = 'h3'; ?>
             <?php include( locate_template('partials/heading_english_espanol.php')); ?>
         </div>
-        <div class="contact-form">
+        <div class="col-sm-12 contact-form">
             <?php gravity_form( 1, false, false, false, '', true ); ?>
         </div>
+        </div>
     </div>
-    <div class="col-sm-4">
-        
+    <div class="home-slide-content-relative">
+        <div class="col-sm-4">
+            <?php $contact_image = get_field('contact_image', 8); ?>
+            <img src="<?php echo $contact_image['sizes']['large']; ?>" alt="<?php echo $contact_image['alt']; ?>">
+        </div>
     </div>
 </div>
--- wp-content/themes/custom/partials/walk-with-us/walk_with_us_callout.php
+++ wp-content/themes/custom/partials/walk-with-us/walk_with_us_callout.php
@@ -3,14 +3,20 @@
         <div class="question-section-header col-sm-8 offset-sm-2 col-xs-12 mb2">
             <div class=" question-section-title">
                 <?php $title = array('english' => 'More Questions', 'espanol' => 'Más preguntas?'); ?>
-                <?php $classes = 'link bold'; ?>
+                <?php $classes = 'bold'; ?>
                 <?php $tag = 'h3'; ?>
                 <?php include( locate_template('partials/heading_english_espanol.php') ); ?>
             </div>
             <div class="row">
-                <div class="col-sm-6 offset-sm-3 mt1">
-                    <p class="link">Email <a href="mailto:plantonmovil@gmail.com">Lucia Monge</a> with questions, comments, or requests for more information!</p>
-                    <p class="link">Envíe un correo electrónico a <a href="mailto:plantonmovil@gmail.com">Lucia Monge</a> con preguntas, comentarios o solicitudes para obtener más información!</p>
+                <div class="col-sm-12 mt1">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <p class="small">Email <a class="link"href="mailto:plantonmovil@gmail.com">Lucia Monge</a> with questions, comments, or requests for more information!</p>
+                        </div>
+                        <div class="col-sm-6">
+                            <p class="small">Envíe un correo electrónico a <a class="link"href="mailto:plantonmovil@gmail.com">Lucia Monge</a> con preguntas, comentarios o solicitudes para obtener más información!</p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
