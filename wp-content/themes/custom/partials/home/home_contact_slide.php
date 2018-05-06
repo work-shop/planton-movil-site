a6f3388ea88fe5cc7b5e6a825346eb239f6822d1
Nic Schumann <nicolas_schumann@brown.edu>
2018-05-06 16:44:25 -0400

    [Footer, Contact] made the footer more legible with the new english / spanish pagenames, guarded the image on the contact form.

--- wp-content/themes/custom/partials/home/home_contact_slide.php
+++ wp-content/themes/custom/partials/home/home_contact_slide.php
@@ -1,22 +1,23 @@
 <div id="contact" class="home-slide row">
     <div class="home-slide-content-relative col-sm-6 offset-sm-1">
         <div class="row">
-
-
-        <div class="col-sm-6 offset-sm-3">
-            <?php $title = array('english' => 'Contact', 'espanol' => 'Contacto'); ?>
-            <?php $tag = 'h3'; ?>
-            <?php include( locate_template('partials/heading_english_espanol.php')); ?>
-        </div>
-        <div class="col-sm-12 contact-form">
-            <?php gravity_form( 1, false, false, false, '', true ); ?>
-        </div>
+            <div class="col-sm-6 offset-sm-3">
+                <?php $title = array('english' => 'Contact', 'espanol' => 'Contacto'); ?>
+                <?php $tag = 'h3'; ?>
+                <?php include( locate_template('partials/heading_english_espanol.php')); ?>
+            </div>
+            <div class="col-sm-12 contact-form">
+                <?php gravity_form( 1, false, false, false, '', true ); ?>
+            </div>
         </div>
     </div>
     <div class="home-slide-content-relative">
+        <?php $contact_image = get_field('contact_image', 8); ?>
+        <?php if ( $contact_image ) : ?>
         <div class="col-sm-4">
-            <?php $contact_image = get_field('contact_image', 8); ?>
+
             <img src="<?php echo $contact_image['sizes']['large']; ?>" alt="<?php echo $contact_image['alt']; ?>">
         </div>
+        <?php endif; ?>
     </div>
 </div>
